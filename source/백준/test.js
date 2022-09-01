fcaa0c46d760a4921bc5a35089f90a38a4d33bf4
euiyeonlee <dmldus1121@naver.com>
2022-09-01 13:21:15 +0900

    add : test

---
+++ source/백준/test.js
@@ -0,0 +1,8 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
+
+console.log(input);
