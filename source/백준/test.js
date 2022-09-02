da253ff65daea710157a7a6c66c40d44d4ef74c2
euiyeonlee <dmldus1121@naver.com>
2022-09-02 13:35:06 +0900

    add : 2839 설탕배달

--- source/백준/test.js
+++ source/백준/test.js
@@ -5,4 +5,22 @@ const input = require("fs")
   .trim()
   .split("\n");
 
-console.log(input);
+let n = input;
+let cnt = 0;
+let arr = [];
+let result = 0;
+
+for (i = 0; i < parseInt(n / 5) + 1; i++) {
+  cnt += i;
+  let remain = n - i * 5;
+
+  if (remain % 3 == 0) {
+    cnt += remain / 3;
+    arr.push(cnt);
+  }
+
+  cnt = 0;
+}
+
+result = arr.length > 0 ? Math.min(...arr) : -1;
+console.log(result);
