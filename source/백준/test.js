41553f0ecfdafc1176b2bb9916b3b9301d2b522b
euiyeonlee <dmldus1121@naver.com>
2022-09-08 23:54:07 +0900

    add : 1914 하노이탑

--- source/백준/test.js
+++ source/백준/test.js
@@ -1,34 +1,86 @@
 const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
 const input = require("fs").readFileSync(filePath).toString().trim().split(" ");
 
-const m = parseInt(input[0]);
-const n = parseInt(input[1]);
-let x = m;
+function solution(n) {
+  const result = [];
 
-let prime = [];
+  function hanoi(n, from, to) {
+    const arr = [];
+    let temp = 1;
+    let by = 6 - from - to;
 
-// 범위가 1아니 2부터일경우 소수 2를 추가해주기위함
-if (m <= 2) prime.push(2);
-// m부터 n까지 확인
-while (x < n + 1) {
-  // x를 2~x-1까지의 숫자로 나눠보기 나눠떨어지면 break; 마지막까지 안나눠떨어지면 prime에 추가
+    while (temp > 0) {
+      while (n > 1) {
+        arr.push(to);
+        arr.push(by);
+        arr.push(from);
+        arr.push(n);
+        n--;
+        arr.push(to);
+        to = by;
+        by = arr.pop();
+      }
 
-  // 제곱근이 있을 경우 판별
-  if (Math.sqrt(x) % 1 === 0) {
-    x++;
-    continue;
-  }
+      result.push([from, to]);
 
-  // 2부터 제곱근+1까지 하나씩 나눠보기
-  for (i = 2; i < Math.sqrt(x) + 1; i++) {
-    // x가 i로 나눠질 경우 판별
-    if (x % i == 0) {
-      break;
+      if (arr.length - 1 > 0) {
+        n = arr.pop();
+        from = arr.pop();
+        by = arr.pop();
+        to = arr.pop();
+        result.push([from, to]);
+        n--;
+        arr.push(from);
+        from = by;
+        by = arr.pop();
+      } else {
+        temp = 0;
+      }
     }
-    //i가 마지막까지 살아있을때 prime 배열에 x 추가 -> 더 좋은 조건문이 있을거같음.
-    if (i > Math.sqrt(x)) prime.push(x);
   }
-  x++;
+
+  hanoi(n, 1, 3);
+  return result;
 }
 
-prime.map((e) => console.log(e));
+solution(input);
+
+/* let cnt = Math.pow(2, input) - 1;
+let result = [];
+
+const hanoi = (n, a, b, c) => {
+  if (n == 0) return;
+  else {
+    hanoi(n - 1, a, c, b);
+    result.push(`${a} ${c}`);
+    hanoi(n - 1, b, a, c);
+  }
+};
+
+function sol(n) {
+  hanoi(n, 1, 2, 3);
+
+  return result;
+}
+
+sol(input);
+
+console.log(cnt);
+result.map((e) => console.log(e)); */
+
+/* let cnt = Math.pow(2, input) - 1;
+let result = [];
+
+function hanoi(n, a, b, c) {
+  if (n == 0) return;
+  else {
+    hanoi(n - 1, a, c, b);
+    result.push(`${a} ${c}`);
+    hanoi(n - 1, b, a, c);
+  }
+}
+
+hanoi(input, 1, 2, 3);
+
+console.log(cnt);
+result.map((e) => console.log(e)); */
